b523a4b4284337ecef0bbf6d2725a3f6494cf97f
Brayan Vilchez Daga <brayandaga@gmail.com>
2023-03-11 18:17:24 +0100

    Product Detail

--- main.js
+++ main.js
@@ -2,9 +2,22 @@ const navEmail = document.querySelector(".navbar-email");
 const menuDesktop = document.querySelector(".desktop-menu");
 const menuMobile = document.querySelector(".mobile-menu");
 const hambMenu = document.querySelector(".menu");
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
 
-navEmail.addEventListener("click", function(){ toogleContent(menuDesktop) });
-hambMenu.addEventListener("click", function(){ toogleContent(menuMobile) });
+navEmail.addEventListener("click", function(){ 
+    productDetail.classList.add("inactive");
+    toogleContent(menuDesktop) 
+});
+hambMenu.addEventListener("click", function(){ 
+    productDetail.classList.add("inactive");
+    toogleContent(menuMobile) 
+});
+shoppingCart.addEventListener("click", function(){
+    menuDesktop.classList.add("inactive");
+    menuMobile.classList.add("inactive");
+    toogleContent(productDetail);
+});
 
 function toogleContent(element){
     return element.classList.toggle("inactive");
