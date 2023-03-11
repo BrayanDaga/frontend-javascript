f08449a9d1eb68605f4887da6c35d797f92e4423
Brayan Vilchez Daga <brayandaga@gmail.com>
2023-03-11 17:21:17 +0100

    mobile-menu

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
 const navEmail = document.querySelector(".navbar-email");
 const menuDesktop = document.querySelector(".desktop-menu");
+const menuMobile = document.querySelector(".mobile-menu");
+const hambMenu = document.querySelector(".menu");
 
-navEmail.addEventListener("click", toggleMenuDesktop);
-function toggleMenuDesktop(){
-    menuDesktop.classList.toggle("inactive");
+navEmail.addEventListener("click", function(){ toogleContent(menuDesktop) });
+hambMenu.addEventListener("click", function(){ toogleContent(menuMobile) });
+
+function toogleContent(element){
+    return element.classList.toggle("inactive");
 }
