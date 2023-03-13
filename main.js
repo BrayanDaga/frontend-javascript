f1569f595fcedf44406faa4a06adda39163761f5
Brayan Vilchez Daga <brayandaga@gmail.com>
2023-03-13 20:39:49 +0100

    list-products

--- main.js
+++ main.js
@@ -4,21 +4,64 @@ const menuMobile = document.querySelector(".mobile-menu");
 const hambMenu = document.querySelector(".menu");
 const shoppingCart = document.querySelector(".navbar-shopping-cart");
 const productDetail = document.querySelector(".product-detail");
+const containerProducts = document.querySelector(".cards-container");
 
-navEmail.addEventListener("click", function(){ 
-    productDetail.classList.add("inactive");
-    toogleContent(menuDesktop) 
+navEmail.addEventListener("click", function () {
+  productDetail.classList.add("inactive");
+  toogleContent(menuDesktop);
 });
-hambMenu.addEventListener("click", function(){ 
-    productDetail.classList.add("inactive");
-    toogleContent(menuMobile) 
+hambMenu.addEventListener("click", function () {
+  productDetail.classList.add("inactive");
+  toogleContent(menuMobile);
 });
-shoppingCart.addEventListener("click", function(){
-    menuDesktop.classList.add("inactive");
-    menuMobile.classList.add("inactive");
-    toogleContent(productDetail);
+shoppingCart.addEventListener("click", function () {
+  menuDesktop.classList.add("inactive");
+  menuMobile.classList.add("inactive");
+  toogleContent(productDetail);
 });
 
-function toogleContent(element){
-    return element.classList.toggle("inactive");
+function toogleContent(element) {
+  return element.classList.toggle("inactive");
 }
+
+const listProduct = [
+  {
+    name: "Bike",
+    price: 120.0,
+    image:
+      "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    name: "Tv",
+    price: 200.0,
+    image:
+      "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+  {
+    name: "Computer",
+    price: 800.0,
+    image:
+      "https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+  },
+];
+
+function renderListProducts() {
+    listProduct.forEach(product => {
+        containerProducts.innerHTML += `<div class="product-card">
+        <img
+          src="${product.image}"
+          alt="">
+        <div class="product-info">
+          <div>
+            <p>${product.price}</p>
+            <p>${product.name}</p>
+          </div>
+          <figure>
+            <img src="./icons/bt_add_to_cart.svg" alt="">
+          </figure>
+        </div>
+        </div>`;
+     });
+}
+
+renderListProducts();
